cf1d958dfe40ebdf9cdc39d723bd8a2ab69f1696
spaceman-dev <dhruv.bhat@yahoo.in>
2020-07-31 12:45:02 +0530

    MissingNumberInArray

---
+++ Arrays/MissingNumberInArray.java
@@ -0,0 +1,30 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class MissingNumberInArray 
+{
+    public static void main (String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        //System.out.println("Enter the Number of Test Cases: ");
+        int t = sc.nextInt();
+        for(int l = 0 ; l < t ; l++)
+        {
+            //System.out.println("Enter the Number of Array Elements: ");
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            //System.out.println("Enter the "+ (n-1) + " Elements in the Array: ");
+            int sum = 0;
+            for(int i = 0 ; i < n-1 ; i++)
+            {
+                arr[i] = sc.nextInt();
+                sum+= arr[i];
+            }
+            int totalsum = n * (n + 1) / 2;
+            System.out.println(totalsum-sum);
+        }
+	}
+}
