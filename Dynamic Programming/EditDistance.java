1c862d2ec2bfc704f883b192d4ec1841e9ab7470
spaceman-dev <dhruv.bhat@yahoo.in>
2020-08-01 16:15:19 +0530

    Edit Distance

---
+++ Dynamic Programming/EditDistance.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class EditDistance
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        //System.out.println("Enter the Number of Test Cases: ");
+        int t = sc.nextInt();
+        for(int i = 0 ; i < t ; i++)
+        {
+            //System.out.println("Enter length of first string: ");
+            int p = sc.nextInt();
+            //System.out.println("Enter length of second string: ");
+            int q = sc.nextInt();
+            //System.out.println("Enter the first string: ");
+            String s1 = sc.next();
+            //System.out.println("Enter  the second string: ");
+            String s2 = sc.next();
+            System.out.println(editDistance(s1, s2, p, q));
+        }
+    }
+    public static int editDistance(String s1, String s2, int p, int q)
+    {
+        int min = 0;
+        if(p == 0)
+            return q;
+        if(q == 0)
+            return p;
+        if(s1.charAt(p-1) == s2.charAt(q-1))
+            editDistance(s1, s2, p-1, q-1);
+        else
+        {
+            int insert = editDistance(s1, s2, p, q-1);
+            int remove = editDistance(s1, s2, p-1, q);
+            int replace = editDistance(s1, s2, p-1, q-1);
+            min = Math.min(insert, Math.min(remove, replace));
+        }
+        return(min + 1);
+    }
+}
