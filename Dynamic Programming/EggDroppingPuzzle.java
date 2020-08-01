076adc9dccf0184a5f9af4b18f57506e92a14008
spaceman-dev <dhruv.bhat@yahoo.in>
2020-08-01 11:52:32 +0530

    Egg Dropping Puzzle

---
+++ Dynamic Programming/EggDroppingPuzzle.java
@@ -0,0 +1,52 @@
+/*package whatever //do not write package name here */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class EggDroppingPuzzle
+{ 
+// Find sum of binomial coefficients xCi 
+// (where i varies from 1 to n). If the sum 
+// becomes more than K 
+static int binomialCoeff(int x, int n, int k) 
+{ 
+    int sum = 0, term = 1; 
+    for (int i = 1; i <= n && sum < k; ++i)
+    { 
+        term *= x - i + 1; 
+        term /= i; 
+        sum += term; 
+    } 
+    return sum; 
+} 
+// Do binary search to find minimum  
+// number of trials in worst case. 
+static int minTrials(int n, int k) 
+{ 
+    // Initialize low and high as 1st  
+    //and last floors 
+    int low = 1, high = k; 
+    // Do binary search, for every mid,  
+    // find sum of binomial coefficients and  
+    // check if the sum is greater than k or not. 
+    while (low < high) 
+    { 
+        int mid = (low + high) / 2; 
+        if (binomialCoeff(mid, n, k) < k) 
+            low = mid + 1; 
+        else
+            high = mid; 
+    } 
+    return low; 
+} 
+public static void main(String args[]) 
+{ 
+    Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+    for(int i = 0 ; i < t ; i++)
+    {
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        System.out.println(minTrials(n, k));
+    }
+} 
+} 
---
+++ Greedy/ActivitySelection.java
@@ -0,0 +1,54 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ActivitySelection
+{
+    public static void main (String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        //System.out.println("Enter Number of test cases: ");
+        int t = sc.nextInt();
+        for(int l = 0 ; l < t ; l++)
+        {
+            //System.out.println("Enter the Number of Activities: ");
+            int n = sc.nextInt();
+            int start[] = new int[n];
+            int end[] = new int[n];
+            //System.out.println("Enter the Start Time of the "+ n + " activities: ");
+            for(int i = 0 ; i < n ; i++)
+                start[i] = sc.nextInt();
+            //System.out.println("Enter the End Times of the " + n + " activities: ");
+            for(int i = 0 ; i < n ; i++)
+                end[i] = sc.nextInt();
+            Arrays.sort(start);
+            Arrays.sort(end);
+            int in = 1;
+            int max = 1;
+            int time = start[0];
+            int i = 1;
+            int j = 0;
+            while(i < n && j < n)
+            {
+                if(start[i] <= end[i])
+                {
+                    in++;
+                    if(in > max)
+                    {
+                        max = in;
+                        time = start[i];
+                    }
+                    i++;
+                }
+                else
+                {
+                    in--;
+                    j++;
+                }
+            }
+            System.out.println(max);
+        }
+	}
+}
