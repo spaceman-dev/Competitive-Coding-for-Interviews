1a760111c6ed00994145a339c8a62e5a92043e2f
spaceman-dev <dhruv.bhat@yahoo.in>
2020-08-03 19:02:37 +0530

    Alternating Characters

---
+++ Strings/AlternatingCharacters.java
@@ -0,0 +1,44 @@
+package Strings;
+import java.io.*;
+import java.util.*;
+
+public class AlternatingCharacters {
+
+    // Complete the alternatingCharacters function below.
+    static int alternatingCharacters(String s) {
+        char check = s.charAt(0); 
+        int count = 0; 
+        for( int i = 1 ; i < s.length() ; i++) 
+        { 
+            if(check == s.charAt(i)) 
+            { 
+                count++; 
+                continue; 
+            } 
+            check = s.charAt(i); 
+        } 
+        return count ; 
+    }
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        // BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        //int q = scanner.nextInt();
+        // scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        //for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            int result = alternatingCharacters(s);
+            System.out.println(result);
+
+        //     bufferedWriter.write(String.valueOf(result));
+        //     bufferedWriter.newLine();
+        //}
+
+        // bufferedWriter.close();
+
+        scanner.close();
+    }
+}
