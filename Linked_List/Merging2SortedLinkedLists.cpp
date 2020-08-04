00ec66758b44f5ba89ee7b8a2b765ed0fee3fe62
spaceman-dev <dhruv.bhat@yahoo.in>
2020-08-04 18:22:14 +0530

    Merging two sorted Linked Lists

---
+++ Linked_List/Merging2SortedLinkedLists.cpp
@@ -0,0 +1,101 @@
+// { Driver Code Starts
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node* sortedMerge(struct Node* a, struct Node* b);
+
+/* Function to print Nodes in a given linked list */
+void printList(struct Node *n)
+{
+    while (n!=NULL)
+    {
+        cout << n->data << " ";
+        n = n->next;
+    }
+    cout << endl;
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+
+        int data;
+        cin>>data;
+        struct Node *head1 = new Node(data);
+        struct Node *tail1 = head1;
+        for (int i = 1; i < n; ++i)
+        {
+            cin>>data;
+            tail1->next = new Node(data);
+            tail1 = tail1->next;
+        }
+
+        cin>>data;
+        struct Node *head2 = new Node(data);
+        struct Node *tail2 = head2;
+        for(int i=1; i<m; i++)
+        {
+            cin>>data;
+            tail2->next = new Node(data);
+            tail2 = tail2->next;
+        }
+
+        Node *head = sortedMerge(head1, head2);
+        printList(head);
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+ 
+
+/* Link list Node
+struct Node {
+  int data;
+  struct Node *next;
+  
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+Node* sortedMerge(Node* head_A, Node* head_B)  
+{  
+    // code here
+    if(head_A == NULL)
+        return head_B;
+    if(head_B == NULL)
+        return head_A;
+    if(head_A->data < head_B->data)
+    {
+        head_A->next = sortedMerge(head_A->next, head_B);
+        return head_A;
+    }
+    if(head_B->data >= head_B->data)
+    {
+        head_B->next = sortedMerge(head_A, head_B->next);
+        return head_B;
+    }
+}
