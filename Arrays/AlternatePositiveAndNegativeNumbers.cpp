5043a2cbe5883a17dcaff6ec8bfc231b9f056c4d
Dhruv Bhat <52482148+spaceman-dev@users.noreply.github.com>
2021-04-17 16:56:08 +0530

    Alternate Positive and Negative Numbers

---
+++ Arrays/AlternatePositiveAndNegativeNumbers.cpp
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+
+	void rearrange(int arr[], int n)
+	{
+	    // code here
+	    vector<int> tempPos, tempNeg;
+	    for(int i = 0 ; i < n ; i++)
+	    {
+	        if(arr[i] >= 0)
+	            tempPos.push_back(arr[i]);
+	        else
+	            tempNeg.push_back(arr[i]);
+	    }
+	    int i = 0, pos = 0, neg = 0;
+	    while(pos < tempPos.size() && neg < tempNeg.size())
+	    {
+	        arr[i++] = tempPos[pos++];
+	        arr[i++] = tempNeg[neg++];
+	    }
+	    while(pos < tempPos.size())
+	        arr[i++] = tempPos[pos++];
+	    while(neg < tempNeg.size())
+	        arr[i++] = tempNeg[neg++];
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.rearrange(arr, n);
+        for (i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
