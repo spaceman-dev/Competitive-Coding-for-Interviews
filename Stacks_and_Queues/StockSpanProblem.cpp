1e004afefc6599a76976b333fd5ee662fc70c8c7
Dhruv Bhat <52482148+spaceman-dev@users.noreply.github.com>
2021-04-27 18:14:59 +0530

    Stock Span Problem

---
+++ Stacks_and_Queues/StockSpanProblem.cpp
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+
+
+class Solution
+{
+    public:
+    //Function to calculate the span of stockâ€™s price for all n days.
+    vector <int> calculateSpan(int arr[], int n)
+    {
+       // Your code here
+       vector<int> v;
+       stack<int> st;
+       for(int i = 0 ; i < n ; i++)
+       {
+            while(!st.empty() && arr[st.top()] <= arr[i])
+                st.pop();
+            if(st.empty())
+                v.push_back(i+1);
+            else
+                v.push_back(i - st.top());
+            st.push(i);
+       }
+       return v;
+    }
+};
+
+
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int i,a[n];
+		for(i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+		Solution obj;
+		vector <int> s = obj.calculateSpan(a, n);
+		
+		for(i=0;i<n;i++)
+		{
+			cout<<s[i]<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
