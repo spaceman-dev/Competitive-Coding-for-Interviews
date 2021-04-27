002df656dded6e836fa8e7a556c39bf9b48b0c42
Dhruv Bhat <52482148+spaceman-dev@users.noreply.github.com>
2021-04-27 18:23:35 +0530

    The Celebrity Problem

---
+++ Arrays/TheCelebrityProblem.cpp
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution 
+{
+    public:
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& M, int n) 
+    {
+        // code here
+        int res = 0;
+        for(int i = 1 ; i < n ; i++)
+        {
+            if(M[res][i] == 1)
+                res = i;
+        }
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(i != res && (M[res][i] == 1 || M[i][res] == 0))
+                return -1;
+        }
+        return res;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > M( n , vector<int> (n, 0));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cin>>M[i][j];
+            }
+        }
+        Solution ob;
+        cout<<ob.celebrity(M,n)<<endl;
+
+    }
+}
+  // } Driver Code Ends
