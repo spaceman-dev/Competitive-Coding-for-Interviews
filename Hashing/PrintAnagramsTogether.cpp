1b0bb15b433bbc2a7ffc2ad5fa64711a7b737a95
Dhruv Bhat <52482148+spaceman-dev@users.noreply.github.com>
2021-04-30 18:53:14 +0530

    Print Anagrams Together

---
+++ Hashing/PrintAnagramsTogether.cpp
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+vector<vector<string> > Anagrams(vector<string>& string_list) ;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+vector<vector<string> > Anagrams(vector<string>& string_list) 
+{
+    // Your Code Here
+    vector<vector<string>> v;
+    unordered_map<string, vector<string>> mp;
+    for(auto it : string_list)
+    {
+        string s = it;
+        sort(s.begin(), s.end());
+        mp[s].push_back(it);
+    }
+    for(auto it : mp)
+        v.push_back(it.second);
+    return v;
+}
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<string> string_list(n);
+        for (int i = 0; i < n; ++i)
+            cin>>string_list[i]; 
+        vector<vector<string> > result = Anagrams(string_list);
+        sort(result.begin(),result.end());
+        for (int i = 0; i < result.size(); i++)
+        {
+            for(int j=0; j < result[i].size(); j++)
+            {
+                cout<<result[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
